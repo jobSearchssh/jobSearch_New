34828e90fe36c30337aa5b5895f9f2b0812199b4
RAY <srtongjitu@163.com>
2015-01-15 21:41:19 +0800

    Initial Commit

---
+++ jobSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jobSearch
+//
+//  Created by RAY on 15/1/15.
+//  Copyright (c) 2015年 麻辣工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jobSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jobSearch
+//
+//  Created by RAY on 15/1/15.
+//  Copyright (c) 2015年 麻辣工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
