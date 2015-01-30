0406d37b5ceda8170a7959ad9060072444b90cb6
puppyxiaoyu <ty195297181@126.com>
2015-01-30 14:49:24 +0800

    20150130 by puppy

---
+++ jobSearch/MLResumeVideoVC.h
@@ -0,0 +1,13 @@
+//
+//  MLResumeVideoVC.h
+//  jobSearch
+//
+//  Created by 田原 on 15/1/30.
+//  Copyright (c) 2015年 麻辣工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLResumeVideoVC : UIViewController
+
+@end
